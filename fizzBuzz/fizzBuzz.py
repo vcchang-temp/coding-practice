96a31a4d6b195b67c9379eb980dc4e091b9e5641
vcchang <vcchang@users.noreply.github.com>
2019-05-10 00:49:34 -0700

    Fix committing folder with all files

---
+++ fizzBuzz/fizzBuzz.py
@@ -0,0 +1,45 @@
+# FizzBuzz: write a program that returns a list of string representations
+# of numbers 1 to n. For multiples of 3, add "Fizz" to the list; for 
+# multiples of 5, add "Buzz" to the list; for multiples of both 3 and 5,
+# add "FizzBuzz" to the list.
+
+# Naive approach
+# Time: O(n) -> must iterate through all nums of n
+# Space: O(1) -> storage bounded by constant
+
+def fizzBuzz(n: int):
+    list = []
+
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            list.append("FizzBuzz")
+        elif i % 3 == 0:
+            list.append("Fizz")
+        elif i % 5 == 0:
+            list.append("Buzz")
+        else:
+            list.append(str(i))
+
+    return list
+
+if __name__ == "__main__":
+    num = 15
+    expected = [
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz"
+    ]
+    assert expected == fizzBuzz(num)
+    print("FizzBuzz success, huzzah!")
