0002f322cc4e0f8aaa39d7e0f2d429fdc3a152d9
vcchang <vcchang@users.noreply.github.com>
2019-05-12 22:43:57 -0700

    Create list-1 solutions file

---
+++ CodingBat/list-1.py
@@ -0,0 +1,6 @@
+# Practicing Python to learn basic syntax
+# https://codingbat.com/python
+
+# List-1
+# Basic python list problems -- no loops.. Use a[0], a[1], ... to access elements in a list, len(a) is the length.
+
