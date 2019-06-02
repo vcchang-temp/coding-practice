5820ca6426e4be9f29b207be7a2acefb1a0d574a
vcchang <vcchang@users.noreply.github.com>
2019-06-02 14:12:35 -0700

    Add three largest numbers brute force solution

---
+++ Searching/threeLargestNumbers/find.py
@@ -0,0 +1,52 @@
+# Three largest numbers
+# (return sorted array of three largest numbers)
+
+# Input: int array, arr
+
+# Brute force approach
+# Time: O(nlogn) -> Python uses Timsort -> O(nlogn)
+#                   which is > runtime of findLargestNum
+#                   helper (O(n)) * 3 = O(3n) ~= O(n)
+# Space: O(n)    -> create array of three nums -> O(n)
+#                   in cases when n <= 3; otherwise more
+#                   like O(1)
+
+from typing import List
+
+def find(arr: List[int]):
+    result = []
+
+    firstLargestNum = findLargestNum(arr)
+    result.append(firstLargestNum)
+    arr.remove(firstLargestNum)
+
+    secondLargestNum = findLargestNum(arr)
+    result.append(secondLargestNum)
+    arr.remove(secondLargestNum)
+
+    thirdLargestNum = findLargestNum(arr)
+    result.append(thirdLargestNum)
+    # technically no need to remove third largest num
+
+    result.sort()
+    return result
+
+def findLargestNum(arr: List[int]):
+    maxNum = arr[0]
+    for num in arr:
+        if num > maxNum:
+            maxNum = num
+    return maxNum
+
+if __name__ == "__main__":
+    arr = [17, 90, 34, 52, 67, 1, 0, 903]
+    expected = [67, 90, 903]
+    actual = find(arr)
+    assert actual == expected
+    print(f"Found the three largest numbers in the array! {actual}")
+
+    arrWithDupes = [17, 5, 17, 1, 903]
+    expectedDupes = [17, 17, 903]
+    actualDupes = find(arrWithDupes)
+    assert actualDupes == expectedDupes
+    print(f"Found the three largest numbers in the array! {actualDupes}")
