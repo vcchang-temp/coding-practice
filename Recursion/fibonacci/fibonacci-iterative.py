5e6f8b5fce3fc6edd92a360a5aa02ec6a5e98968
vcchang <vcchang@users.noreply.github.com>
2019-06-03 19:27:36 -0700

    Add fibonacci iterative solution

---
+++ Recursion/fibonacci/fibonacci-iterative.py
@@ -0,0 +1,42 @@
+# Fibonacci
+
+# Input: int n
+
+# Time: O(n)  -> iterate through each int of n
+# Space: O(1) -> space is not growing according to n
+
+def findNthFibNum(n: int):
+    if n <= 1:
+        return n
+    total, count, prevTotal = 1, 2, 0 # prevTotal = fib(0), total = fib(1)
+    while count <= n: # n-1 iterations total b/c already calculated fib(1)
+        temp = total
+        total += prevTotal
+        prevTotal = temp
+        count += 1
+    return total
+        
+if __name__ == "__main__":
+    nZero = 0
+    expectedZero = 0
+    actualZero = findNthFibNum(nZero)
+    assert actualZero == expectedZero
+    print(f"The {nZero}th Fibonacci number is {actualZero}.")
+
+    nOne = 1
+    expectedOne = 1
+    actualOne = findNthFibNum(nOne)
+    assert actualOne == expectedOne
+    print(f"The {nOne}st Fibonacci number is {actualOne}.")
+
+    n = 6
+    expected = 8
+    actual = findNthFibNum(n)
+    assert actual == expected
+    print(f"The {n}th Fibonacci number is {actual}.")
+
+    nLarge = 17
+    expectedLarge = 1597
+    actualLarge = findNthFibNum(nLarge)
+    assert actualLarge == expectedLarge
+    print(f"The {nLarge}th Fibonacci number is {actualLarge}.")
