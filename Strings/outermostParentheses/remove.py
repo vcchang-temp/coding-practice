c4faec8c7da46d276a32991ae18669cbe1349e9e
vcchang <vcchang@users.noreply.github.com>
2019-06-20 10:34:00 -0700

    WIP remove outermost parentheses

---
+++ Strings/outermostParentheses/remove.py
@@ -0,0 +1,10 @@
+# Remove outermost parentheses
+# (of every primitive valid parentheses string)
+
+# Input: string s that's either empty or a string 
+#        with one or more valid parentheses strings
+#        (ie: a string that has at least one 
+#        primitive parentheses -> "()")
+
+def remove(s: str):
+    return None
