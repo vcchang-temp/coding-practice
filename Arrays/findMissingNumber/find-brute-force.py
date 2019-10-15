3881432e10d67750cc34e4a27d6348191f0faeb1
vcchang <vcchang@users.noreply.github.com>
2019-10-15 12:09:30 -0700

    WIP Fix brute force solution

--- Arrays/findMissingNumber/find-brute-force.py
+++ Arrays/findMissingNumber/find-brute-force.py
@@ -1,23 +1,33 @@
+# WIP
+
 # Find missing number in array
 # Array is of len n and elements are in range 1 to n+1
 
 # Brute force (looping) approach
-# Time: O(n^2) -> worst case: reversed order -> n-1, n-2,..., 1
-#                 elements to iterate through for each loop iteration
-#                 = n(n-1)/2 ~= O(n^2)
+# Time: O(n^2) -> loop through array of len n for each element
 # Space: O(1)  -> space does not grow with input
 
 from typing import List
 
 def find(arr: List[int]):
     currNum = 1
-    for i in range(len(arr)):
-        if currNum == arr[i]:
+    haveSeenLastNum = False
+    for j in range(len(arr)):
+        if currNum == arr[j]:
             currNum += 1
+            if arr[j] == len(arr) + 1:
+                haveSeenLastNum = True
             continue
-        elif i == len(arr)-1:
-            return currNum
-        currNum += 1
+        elif j == len(arr)-1 and not haveSeenLastNum:
+            return currNum + 1
+        for i in range(len(arr)):
+            if currNum == arr[i]:
+                currNum += 1
+                if arr[j] == len(arr) + 1:
+                    haveSeenLastNum = True
+                break
+            elif i == len(arr) - 1:
+                return currNum
 
 if __name__ == "__main__":
     arr = [1, 4, 5, 8, 2, 3, 6]
@@ -28,6 +38,18 @@ if __name__ == "__main__":
 
     arr = [1, 2, 4]
     expected = 3
+    actual = find(arr)
+    assert actual == expected
+    print(f"Awesome, we found the missing number {actual}!")
+
+    arr = [2]
+    expected = 1
+    actual = find(arr)
+    assert actual == expected
+    print(f"Awesome, we found the missing number {actual}!")
+
+    arr = [1]
+    expected = 2
     actual = find(arr)
     assert actual == expected
     print(f"Awesome, we found the missing number {actual}!")
