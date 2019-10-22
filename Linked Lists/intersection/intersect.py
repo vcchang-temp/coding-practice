9e2d6b981d4a5cbf2baded8673a025a2539bf7d2
vcchang <vcchang@users.noreply.github.com>
2019-10-22 13:14:34 -0700

    Edit solution

--- Linked Lists/intersection/intersect.py
+++ Linked Lists/intersection/intersect.py
@@ -2,40 +2,45 @@
 # Return first intersecting node
 # Assume guaranteed that an intersection exists
 
-# Time: O(max(m, n)) -> m = len of a, n = len of b
-# Space: O(1)        -> space needed doesn't scale with given inputs
+# Time: O(m + n) -> m = len of list a, n = len of list b
+#                   in worst case, ptr belonging to shorter list
+#                   travels to end of shorter and longer lists
+#                   when no intersection
+# Space: O(1)    -> space needed doesn't scale with given inputs
+# Explanation -> imagine if the two ptrs had started at the same distance 
+#                from the intersection, they would reach the intersection 
+#                at the same time, and that's what we want to achieve here
+#                with two ptrs. When switching to the longer list, the
+#                shorter-list ptr has to travel the difference in length 
+#                between the two lists while "waiting" for the longer-list
+#                ptr to reach the end of its list; when the longer-list ptr 
+#                switches to the shorter list, the two ptrs will be at equal 
+#                distances away from the intersection
 
 class Node:
     def __init__(self, x):
         self.val = x
         self.next = None
 
-def intersect(a: Node, b: Node):
-    if not a or not b:
+def intersect(headA: Node, headB: Node):
+    if not headA or not headB:
         return None
 
-    slow = a
-    fast = b
-    while slow.val != fast.val:
-        fast = fast.next
-        if not fast:
-            fast = b
-        elif fast.val == slow.val:
-            return slow
-        slow = slow.next
-        if not slow:
-            slow = a
-    return slow
+    ptrA, ptrB = headA, headB
+    while ptrA != ptrB: # exit loop when ptrA, ptrB are None
+        ptrA = ptrA.next if ptrA else headB
+        ptrB = ptrB.next if ptrB else headA
+    return ptrA
 
-if __name__ == "__main__":
+if __name__ == "__main__": # TODO Fix examples (alg is right)
     head = Node(1)
     head.next = Node(2)
     head.next.next = Node(3)
     head.next.next.next = Node(4)
     head2 = Node(5)
-    head2.next = Node(4)
-    head2.next.next = Node(1)
-    expected = 4
+    head2.next = Node(3)
+    head2.next.next = Node(4)
+    expected = 3
     actual = intersect(head, head2)
     assert actual.val == expected
     print(f"Yay, found the intersecting node: {actual.val}")
@@ -43,7 +48,7 @@ if __name__ == "__main__":
     head = Node(2)
     head2 = Node(5)
     head2.next = Node(2)
-    head2.next.next = Node(1)
+    head2.next.next = Node(2)
     expected = 2
     actual = intersect(head, head2)
     assert actual.val == expected
@@ -51,7 +56,7 @@ if __name__ == "__main__":
 
     head = Node(4)
     head.next = Node(2)
-    head.next.next = Node(3)
+    head.next.next = Node(1)
     head2 = Node(5)
     head2.next = Node(7)
     head2.next.next = Node(4)
