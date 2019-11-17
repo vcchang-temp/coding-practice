b4ad7151e2604497f786d5ec13a08012fd45dcc8
vcchang <vcchang@users.noreply.github.com>
2019-11-17 14:19:34 -0800

    Add solution

---
+++ Misc/confusingNumber/confusingNumber.py
@@ -0,0 +1,47 @@
+# Given an array of N numbers from 1 to N. A number is confusing 
+# if it becomes *another* number *in the array* after we rotate each 
+# number in the digit by 180 degrees. 
+
+# Only 0, 1, 6, 8, 9 are still valid numbers after rotation by 
+# 180 degrees. Other digits are not valid after the rotation.
+
+# Find the total number of the confusing numbers in this array.
+
+# Follow-up: How do you find the total number of confusing numbers 
+# in the array efficiently if N is big?
+
+from typing import List
+
+def countConfusingNums(digits: List[int]) -> int:
+    count = 0
+    for d in digits:
+        if isValidNum(d, len(digits)):
+            count += 1
+    return count
+
+def isValidNum(digit: int, upperBound: int) -> int:
+    validNums = {
+        "0" : "0",
+        "1" : "1",
+        "6" : "9",
+        "8" : "8",
+        "9" : "6"
+    }
+
+    dStr = str(digit)
+    confusingNum = []
+    for n in dStr:
+        if n in validNums:
+            confusingNum.append(validNums[n])
+        else:
+            return False
+
+    confusingNum = int("".join(confusingNum))
+    return confusingNum != digit and confusingNum >= 1 and confusingNum <= upperBound
+
+if __name__ == "__main__":
+    digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    expected = 4 # [6, 9, 16, 19]
+    actual = countConfusingNums(digits)
+    assert actual == expected
+    print(f"There are {actual} confusing numbers in {digits}")
